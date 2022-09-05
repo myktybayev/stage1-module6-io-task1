a810dcf1edcf71700be83918fd5b054b5ba10dcf
Mykty <baha@Bahas-MacBook-Pro.local>
2022-09-05 19:21:11 +0600

    file done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,40 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profle = null;
+        try(FileInputStream fileInputStream = new FileInputStream(file.getPath())) {
+            int ch = fileInputStream.read();
+            String word = "", name ="", email="", age="", phone="";
+            while(ch != -1) {
+                word = word + ""+(char)ch;
+                ch = fileInputStream.read();
+
+                if(ch=='\n'){
+                    if(word.contains("Name")){
+                        name = word.substring(word.indexOf(' ')+1);
+                    }else if(word.contains("Email")) {
+                        email = word.substring(word.indexOf(' ')+1);
+                    }else if(word.contains("Age")) {
+                        age = word.substring(word.indexOf(' ')+1);
+                    }else if(word.contains("Phone")) {
+                        phone = word.substring(word.indexOf(' ')+1);
+                        profle = new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
+                    }
+
+                    word = "";
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+
+        return profle;
     }
 }
